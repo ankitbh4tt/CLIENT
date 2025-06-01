92c32912f242c773d0b3b47739ac4a04e5f8e58a
ankitbh4tt <bhattankit152207@gmail.com>
2025-06-01 16:13:04 +0530

    Add router and react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Todos from './components/Todos'
-import { Route, Routes } from 'react-router-dom'
-import ExpensePage from './pages/ExpensePage'
+import "./App.css";
+import Todos from "./components/Todos";
+import { Route, RouterProvider, Routes } from "react-router-dom";
+import ExpensePage from "./pages/ExpensePage";
+import router from "./Router";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='bg-red-200 min-h-screen'>
-      {/* <Todos/> */}
-      <Routes>
-        <Route path='/todos' element={<Todos/>} />
-        <Route path='/expense' element={<ExpensePage/>}>
-
-        </Route>
-        <Route path='*' element={<ExpensePage/>}/>
-      </Routes>
-    </div>
-  )
+    <>
+      <Toaster position="top-right" />
+      <RouterProvider router={router} />
+    </>
+    // <div className="bg-red-200 min-h-screen">
+    //   {/* <Todos/> */}
+    //   <Routes>
+    //     <Route path="/todos" element={<Todos />} />
+    //     <Route path="/expense" element={<ExpensePage />}></Route>
+    //     <Route path="*" element={<ExpensePage />} />
+    //   </Routes>
+    // </div>
+  );
 }
 
-export default App
+export default App;
