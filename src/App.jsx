3029eb5025bd27e7a0f521f2b2cf75a72560caa5
ankitbh4tt <bhattankit152207@gmail.com>
2025-06-23 22:03:15 +0530

    Add Infinity loader and fixed the solution of infinite api call by removing wrong ternary call of loading context

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
 import "./App.css";
-import Todos from "./components/Todos";
-import { Route, RouterProvider, Routes } from "react-router-dom";
-import ExpensePage from "./pages/ExpensePage";
+import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 import { Toaster } from "react-hot-toast";
-import LoadingProvider, { useLoading } from "./context/loadingContext";
+import { useLoading } from "./context/loadingContext";
 import { useEffect } from "react";
 import { injectLoadingSetter } from "./utils/axios";
+// eslint-disable-next-line no-shadow-restricted-names
+import { Infinity } from "ldrs/react";
+import "ldrs/react/Infinity.css";
 
 function App() {
   const { loading, setLoading } = useLoading();
 
   useEffect(() => {
     injectLoadingSetter(setLoading);
-  }, [setLoading]);
+  }, []);
+
   return (
     <>
-      {loading && <p>Loading...</p>}
-      <>
-        <Toaster position="top-right" />
-        <LoadingProvider>
-          <RouterProvider router={router} />
-        </LoadingProvider>
-      </>
+      <Toaster position="top-right" />
+      {/* Overlay loader */}
+      {loading && (
+        <div className="fixed inset-0 z-50 bg-white bg-opacity-70 flex items-center justify-center">
+          <Infinity
+            size="55"
+            stroke="4"
+            strokeLength="0.15"
+            bgOpacity="0.1"
+            speed="1.3"
+            color="black"
+          />
+        </div>
+      )}
+      <RouterProvider router={router} />
     </>
-    // <div className="bg-red-200 min-h-screen">
-    //   {/* <Todos/> */}
-    //   <Routes>
-    //     <Route path="/todos" element={<Todos />} />
-    //     <Route path="/expense" element={<ExpensePage />}></Route>
-    //     <Route path="*" element={<ExpensePage />} />
-    //   </Routes>
-    // </div>
   );
 }
 
