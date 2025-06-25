0aa3f291588de4ee3d49d8379c3dc4f27ce2705f
ankitbh4tt <bhattankit152207@gmail.com>
2025-06-26 00:18:34 +0530

    Add sidebar as layout,render in every page, Add Tranaction page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
-import FinanceTrackerHome from "./pages/ExpensePage";
 import NotFound from "./components/error/NotFound";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProtectedOutlet from "./layout/ProtectedLayout"; // 👈 Only outlet now
+import SidebarLayout from "./layout/SidebarLayout";
+import Dashboard from "./components/expense/Dashboard";
+import TransactionPage from "./components/expense/transaction/TransactionPage";
 
 const router = createBrowserRouter([
   {
-    path: "/home/dashboard",
+    path: "/home",
     element: <ProtectedOutlet />,
     children: [
       {
         path: "",
-        element: <FinanceTrackerHome />,
+        element: <SidebarLayout />,
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "transactions",
+            element: <TransactionPage />,
+          },
+        ],
       },
     ],
   },
